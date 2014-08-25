9b4ce8117d0c8974257adfb664f7dea0a730cdd5
Janus <sunajd@googlemail.com>
2014-08-25 02:55:30 +0200

    initial commit

---
+++ core/src/de/druz/quadtris/MainMenuScreen.java
@@ -0,0 +1,8 @@
+package de.druz.quadtris;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class MainMenuScreen extends ScreenAdapter {
+
+}
