0d74fad7137d33296cac92e714f5513011948c72
Janus <sunajd@googlemail.com>
2014-08-25 03:29:34 +0200

    cleanup

--- core/src/de/druz/quadtris/MainMenuScreen.java
+++
@@ -1,8 +0,0 @@
-package de.druz.quadtris;
-
-import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-
-public class MainMenuScreen extends ScreenAdapter {
-
-}
